493566cfa22a964638c78962673c5c87ebecfa1b
franklin142 <franklingranados142@gmail.com>
2021-05-10 18:54:26 -0600

    seccion 10: capitulo 77 terminado

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,9 +5,9 @@
  -- estos datos seran eliminados al ejecutar nuevamente el proyecto.
 
 
-INSERT INTO `db_springboot_backend`.`clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin','Flores','franklin1@gmail.com','2021-02-02');
-INSERT INTO `db_springboot_backend`.`clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin2','Flores2','franklin12@gmail.com','2021-02-02');
-INSERT INTO `db_springboot_backend`.`clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin3','Flores3','franklin13@gmail.com','2021-02-02');
-INSERT INTO `db_springboot_backend`.`clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin4','Flores4','franklin14@gmail.com','2021-02-02');
-INSERT INTO `db_springboot_backend`.`clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin5','Flores5','franklin15@gmail.com','2021-02-02');
-INSERT INTO `db_springboot_backend`.`clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin6','Flores6','franklin16@gmail.com','2021-02-02');
+INSERT INTO `clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin','Flores','franklin1@gmail.com','2021-02-02');
+INSERT INTO `clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin2','Flores2','franklin12@gmail.com','2021-02-02');
+INSERT INTO `clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin3','Flores3','franklin13@gmail.com','2021-02-02');
+INSERT INTO `clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin4','Flores4','franklin14@gmail.com','2021-02-02');
+INSERT INTO `clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin5','Flores5','franklin15@gmail.com','2021-02-02');
+INSERT INTO `clientes` (`nombre`,`apellido`,`email`,`created_at`) VALUES ('Franklin6','Flores6','franklin16@gmail.com','2021-02-02');
